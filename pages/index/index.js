8641ca007ee8987c5c79d8b3d5843be5d5359593
KangFusheng <kang.fusheng@outlook.com>
2019-04-10 22:59:32 +0800

    3-11实现下拉刷新
    配置 backgroundTextStyle 为 dark，可以将下拉刷新时的...显示出来

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,47 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
 
-Page({})
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: ""
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      }
+    })
+  }
+})
